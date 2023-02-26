964232234d4aa0ffbaa4b8b4b6a3b305c95aa184
«Elena <helenvit599@gmail.com>
2023-02-26 13:21:11 +0500

    Finished Android Lifecycle app

--- app/src/main/java/ru/synergy/fragmenlifecycleexample/MainActivity.java
+++ app/src/main/java/ru/synergy/fragmenlifecycleexample/MainActivity.java
@@ -1,14 +1,62 @@
 package ru.synergy.fragmenlifecycleexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import ru.synergy.fragmenlifecycleexample.fragments.FirstFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button fragmentButton1, fragmentButton2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentButton1 = (Button) findViewById(R.id.buttonFragment1);
+        fragmentButton2 = (Button) findViewById(R.id.buttonFragment2);
+
+        // Fragment specific
+        // получение фрагмент менеджера, чтобы работать с фрагментами
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        androidx.fragment.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        //добавляем фрагмент на экран
+        FirstFragment firstFragment = FirstFragment.newInstance("1", "2");
+        fragmentTransaction.add(R.id.container, firstFragment);
+        fragmentTransaction.commit();
+
+
+        fragmentButton1.setOnClickListener(onButtonClickListner);
+        fragmentButton2.setOnClickListener(onButtonClickListner);
+    }
+
+    Button.OnClickListener onButtonClickListner = new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+
+            Fragment newFragment = null;
+
+            if(v == fragmentButton1){
+                newFragment = new FirstFragment();
+            }else {
+                newFragment = SecondFragment();
+            }
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            transaction.replace(R.id.container, newFragment);
+            transaction.addToBackStack(null);
+
+            transaction.commit();
+        }
+
     }
 }
--- app/src/main/java/ru/synergy/fragmenlifecycleexample/fragments/SecondFragment.java
+++ app/src/main/java/ru/synergy/fragmenlifecycleexample/fragments/SecondFragment.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+
 import ru.synergy.fragmenlifecycleexample.R;
 
 /**
